a892b10300a441bfb1b40c55e4ddfc73a012d1e8
Jason <r2aushan@gmail.com>
2024-01-09 21:38:02 +0545

    commit on feature branch

---
+++ user.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("to check for server & client");
+    int a=45;
+    int x;
+    printf("Enger a number");
+    scanf("%d",&x);
+    printf("%d",&a);
+    return 0;
+}
